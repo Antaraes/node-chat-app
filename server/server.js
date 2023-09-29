065637378de1177a65f856bfec8c20ec3d4226c5
Min Bhone Thant <95273099+Antaraes@users.noreply.github.com>
2023-09-29 14:24:55 +0630

    Add Chat Feature

--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
 const cors = require("cors");
+const socketIO = require("socket.io");
+
 const publicPath = path.join(__dirname, "../client");
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(cors());
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.broadcast.emit("hi");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
 app.use(express.static("client"));
 const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log("Server is up on port 3000");
+server.listen(port, () => {
+  console.log("Server is up on " + port);
 });
 
 console.log(__dirname + "/../client");
